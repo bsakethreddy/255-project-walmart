096ba6b2d07f2bb140daaf5459076b31c3082439
Saketh Reddy <sakethreddy.333@gmail.com>
2021-05-10 23:25:09 -0700

    UI and flask.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
-from flask import Flask, render_template
+from sklearn.model_selection import train_test_split
+import xgboost as xgb
+from flask import Flask, render_template, request
 import pickle
 
 app = Flask(__name__)
@@ -8,15 +10,34 @@ filename = 'finalized_model.sav'
 loaded_model = pickle.load(open(filename, 'rb'))
 fet = pd.read_csv('joined_data_refined.csv')
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# @app.route('/predict',methods=['POST'])
-# def predict():
-#     result = loaded_model.score(X_test, Y_test)
-#     print(result)
-#     return render_template('index.html', output=result)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    store = request.form.get('store')
+    dept = request.form.get('dept')
+    date = request.form.get('date')
+    isHoliday = request.form.get('isholiday')
+
+    X_test = pd.DataFrame({'Store': [21], 'Dept': [22]})
+
+    print("X_test = ", X_test.head())
+    print("type of X_test = ", type(X_test))
+    y_pred = loaded_model.predict(X_test)
+    print("predict = ", store, dept, date, isHoliday, y_pred)
+    return y_pred
+    pass
+
+# result = loaded_model.score(X_test, Y_test)
+# print(result)
+# return render_template('index.html', output=result)
+
+# @app.route('/submit', methods=['POST'])
+# def getValues():
 
 
 if __name__ == "__main__":
--- xgb.py
+++ xgb.py
@@ -11,7 +11,8 @@ from sklearn.model_selection import GridSearchCV
 import pickle
 
 
-df =  pd.read_csv("joined_data_refined.csv",keep_default_na=False, na_values=[""])
+df = pd.read_csv("joined_data_refined.csv",
+                 keep_default_na=False, na_values=[""])
 print(df.columns)
 print(df.info())
 
@@ -25,23 +26,28 @@ del df['Date']
 del df['IsHoliday']
 del df['dayofweek_name']
 del df['Unnamed: 0']
-df.drop('Type_B',axis=1,inplace=True)
+df.drop('Type_B', axis=1, inplace=True)
 
-X=df[df.columns.difference(['Weekly_Sales'])]
-y=df['Weekly_Sales']
+# X = df[df.columns.difference(['Weekly_Sales'])]
+X = df[['Store', 'Dept']]
+y = df['Weekly_Sales']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42)
 
-xg_reg = xgb.XGBRegressor(learning_rate=0.1, max_depth=6,objective='reg:squarederror')
+xg_reg = xgb.XGBRegressor(learning_rate=0.1, max_depth=6,
+                          objective='reg:squarederror')
 xg_reg.fit(X_train, y_train)
 
-pred=xg_reg.predict(X_train)
-y_pred=xg_reg.predict(X_test)
+pred = xg_reg.predict(X_train)
+y_pred = xg_reg.predict(X_test)
 
-print('Mean Absolute Error:', mean_absolute_error(y_test, y_pred))  
-print('Mean Squared Error:', mean_squared_error(y_test, y_pred))  
+print("xg_reg X_test y_pred = ", type(X_test), X_test.head(), y_pred)
+
+print('Mean Absolute Error:', mean_absolute_error(y_test, y_pred))
+print('Mean Squared Error:', mean_squared_error(y_test, y_pred))
 print('Root Mean Squared Error:', np.sqrt(mean_squared_error(y_test, y_pred)))
-print("R-squared score:",r2_score(y_test, y_pred))
+print("R-squared score:", r2_score(y_test, y_pred))
 print(xg_reg.score(X_test, y_test)*100)
 
 filename = 'finalized_model.sav'
